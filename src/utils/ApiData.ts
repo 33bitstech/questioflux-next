a261e3fc3983693fc8665cfd5e50d4886433e1b2
gusta <ovatsugavlis15@gmail.com>
2025-06-14 22:11:07 -0300

    adicionando função para requisiçoes com fetch, e requisições login, register

---
+++ src/utils/ApiData.ts
@@ -0,0 +1,27 @@
+import { env } from "@/env";
+
+import { HeaderType, BodyInit, Methods, Cache} from "@/types/fetchTypes";
+
+interface IApiDataParams {
+    path: string,
+    method: Methods,
+    body?: BodyInit,
+    headerKey?: HeaderType,
+    headerValue?: string,
+    cache: Cache
+}
+export default async function ApiData({
+    path, body, cache, 
+    method, headerKey, headerValue
+} : IApiDataParams) {
+    const baseUrl = `${env.DOMAIN_API}/${path}`,
+        headers = new Headers()
+    
+    if (headerKey && headerValue) headers.append(headerKey, headerValue)
+    
+    const config = {
+        body, method, ...cache, headers
+    }    
+
+    return await fetch(baseUrl, config)
+}
