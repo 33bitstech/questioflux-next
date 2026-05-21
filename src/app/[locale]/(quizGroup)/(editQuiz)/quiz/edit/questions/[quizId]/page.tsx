d9754194cd8246bbefb5db3ebe9d8fb972ed1697
gusta <ovatsugavlis15@gmail.com>
2026-05-21 19:00:01 -0300

    debug

--- src/app/[locale]/(quizGroup)/(editQuiz)/quiz/edit/questions/[quizId]/page.tsx
+++ src/app/[locale]/(quizGroup)/(editQuiz)/quiz/edit/questions/[quizId]/page.tsx
@@ -7,7 +7,6 @@ import { Metadata } from 'next'
 import { getTranslations } from 'next-intl/server'
 import { cookies } from 'next/headers'
 import { getCookieHeader } from '@/utils/getCookieHeader'
-import { getQuiz } from '../../[quizId]/page'
 
 interface IProps{
     params:Promise<{
@@ -15,6 +14,21 @@ interface IProps{
         quizId: string
     }>
 }
+
+async function getQuiz(quizId: string, cookieHeader: string): Promise<IQuizes | undefined | { err: any }> {
+    try {
+        const response = await fetch(`${env.NEXT_PUBLIC_DOMAIN_FRONT}/api/quiz/${quizId}/auth`, {
+            method: 'GET',
+            headers: { 'cookie': cookieHeader },
+        });
+        const res = await response.json();
+        if (res.message) return { err: res }
+        return res.quiz;
+    } catch (err: any) {
+        console.log(err)
+    }
+}
+
 export async function generateMetadata({ params }: IProps): Promise<Metadata> {
     const {locale, quizId} = await params
     const t = await getTranslations({ locale, namespace: 'editQuizFlow.questionsPage' });
@@ -35,10 +49,9 @@ export async function generateMetadata({ params }: IProps): Promise<Metadata> {
 
 export default async function EditingQuiz({params}:IProps) {
     const {quizId, locale} = await params
-    const cookieStore = await cookies();
-
-const token = cookieStore.get('token')?.value;
-    const quiz = await getQuiz(quizId, `${token}`)    
+    const cookieStore = await cookies()
+    const cookieHeader = getCookieHeader(cookieStore.getAll())
+    const quiz = await getQuiz(quizId, cookieHeader)    
 
     const t = await getTranslations({ locale, namespace: 'editQuizFlow.questionsPage' })
     
