e101f9191628dde247ba3fba0deae497d2860424
gusta <ovatsugavlis15@gmail.com>
2026-06-20 22:43:25 -0300

    fix

--- src/app/[locale]/(emailChange)/email-confirm/action.ts
+++ src/app/[locale]/(emailChange)/email-confirm/action.ts
@@ -1,15 +1,21 @@
 'use server'
 import { env } from "@/env"
+import { getCookieHeader } from "@/utils/getCookieHeader"
+import { cookies } from "next/headers"
 
 export async function confirmEmailChange(token: string) {
+    const cookieStore = await cookies()
+    const cookieHeader = getCookieHeader(cookieStore.getAll())
+
     const response = await fetch(`${env.NEXT_PUBLIC_DOMAIN_API}/email-by-token`, {
         method: 'PUT',
         headers: {
-            'Content-Type': 'application/json'
+            'Content-Type': 'application/json',
+            'cookie': cookieHeader
         },
         body: JSON.stringify({ token })
     })
-    console.log(response)
+
     if (!response.ok) {
         return { err: true }
     }
